4fe93207c39145c004e6d5e35c557da73e4d6218
tedbu <124395912+tedbu@users.noreply.github.com>
2023-11-11 12:44:15 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 let sonor = 0
+basic.showIcon(IconNames.Sword)
 wuKong.setAllMotor(-100, -100)
 basic.forever(function () {
-    sonor = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_mm, DigitalPin.P0)
-    if (0 < 0 && 0 > 0) {
+    sonor = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
+    if (sonor < 20 && sonor > 1) {
         wuKong.setAllMotor(-50, 100)
+        basic.showNumber(sonor)
         basic.pause(randint(0, 10))
     } else {
         wuKong.setAllMotor(-100, -100)
