7b176a03c7d0f5863b99c08f0b183351bc750e07
tedbu <124395912+tedbu@users.noreply.github.com>
2023-11-11 13:55:52 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,15 @@
 let sonor = 0
 basic.showIcon(IconNames.Sword)
-wuKong.setAllMotor(-100, -100)
+wuKong.setAllMotor(-80, -80)
 basic.forever(function () {
-    sonor = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
-    if (sonor < 20 && sonor > 1) {
+    sonor = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_mm, DigitalPin.P1)
+    if (sonor < 200 && sonor > 10) {
         wuKong.setAllMotor(-50, 100)
-        basic.showNumber(sonor)
+        basic.pause(randint(0, 10))
+    } else if (sonor < 10) {
+        wuKong.setAllMotor(10, 10)
         basic.pause(randint(0, 10))
     } else {
-        wuKong.setAllMotor(-100, -100)
+        wuKong.setAllMotor(-80, -80)
     }
 })
