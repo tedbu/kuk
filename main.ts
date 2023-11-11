9195877515dc259b82638acb66ba66e33ac7b97c
tedbu <124395912+tedbu@users.noreply.github.com>
2023-11-11 16:04:07 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,23 @@
+input.onButtonPressed(Button.A, function () {
+    wuKong.setAllMotor(-100, -100)
+})
+input.onButtonPressed(Button.AB, function () {
+    wuKong.stopMotor(wuKong.MotorList.M2)
+    wuKong.stopMotor(wuKong.MotorList.M1)
+})
+input.onButtonPressed(Button.B, function () {
+    wuKong.setLightMode(wuKong.LightMode.BREATH)
+})
 let sonor = 0
 basic.showLeds(`
-    . # # . .
-    # # # . .
-    . # # # #
-    . # # # .
+    # # . # #
+    # # . # #
     . . . . .
+    # . # . #
+    . # . # .
     `)
-basic.showString("Hello!How are you?I'm good thanks bye! ")
-wuKong.setAllMotor(-50, -50)
+basic.showString("Hello!How are you?I'm good.  ")
+wuKong.setAllMotor(-26, -26)
 basic.forever(function () {
     sonor = sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P1)
     if (sonor < 20 && sonor > 2) {
@@ -15,6 +25,7 @@ basic.forever(function () {
         wuKong.setAllMotor(-25, 50)
         basic.pause(randint(20, 50))
     } else {
-        wuKong.setAllMotor(-50, -50)
+        wuKong.setAllMotor(-26, -26)
     }
+    basic.showString("Bye!")
 })
